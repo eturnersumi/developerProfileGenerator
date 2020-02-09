dc37cf6b14cbd0a89afe4deb6c832e5b728b254a
Impactful Actions <eturner.et3@outlook.com>
2020-02-09 01:09:59 -0800

    adding requiring dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {}
+
+init();
